20f421ebd579af2b19daf4ffd452ea72c1af0a29
Willian Andrade <willianandrade953@gmail.com>
2021-07-27 21:31:29 -0300

    Refatoreted

--- public/script.js
+++ public/script.js
@@ -4,21 +4,60 @@ const messages = document.querySelector('.messages-output');
 const form = document.getElementById('message-area');
 const input = document.getElementById('message'); 
 
+var username = '';/* The username */
+
+/* Inserting the user data camp */
+window.onload = () => {
+    const nameContainer = document.querySelector('.name-container');
+    const nameInput = document.getElementById('username');
+    const button = nameContainer.querySelector('button');
+
+    button.addEventListener('click', () => {
+        if (nameInput.value) {
+            username = nameInput.value;
+            nameContainer.remove();
+        };
+    });
+};
+
+/* Messages only to the sender */
 function createSenderMessage(value) {
     const div = document.createElement('div');
 
     messages.appendChild(div);
     div.innerHTML = value;
     div.className = 'my-message';
-}
+};
 
+/* Messages for all users except the sender */
 function createReceivedMessage(msg) {
     const div = document.createElement('div');
 
     messages.appendChild(div);
     div.className = 'other-message';
     div.textContent = msg;
-}
+};
+
+function updateUsersCount(total) {
+    try {
+        document.querySelector('.clients-count').querySelector('nobr').remove();
+    } catch (error) {}
+
+    const p = document.createElement('nobr');
+    const container = document.querySelector('.clients-count');
+
+    container.appendChild(p);
+    if (window.innerWidth <= 500) return p.innerHTML = total;
+    p.innerHTML = `${total} User(s) Online`;
+}; 
+
+/* if any user type */
+input.addEventListener('keyup', () => {
+    if (input.value) {
+        socket.emit('typing', username)
+    }
+    else socket.emit('not typing')
+});
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -26,12 +65,20 @@ form.addEventListener('submit', function(e) {
     if (input.value) {
     createSenderMessage(input.value);
 
-    socket.emit('chat message', input.value);
+    socket.emit('chat message', input.value, username);
 
     input.value = '';
     };
 });
 
+socket.on("typing", user => {
+    document.querySelector('.typing').innerHTML = `${user} is typing...`
+});
+
 socket.on("message for all users", msg => {
     createReceivedMessage(msg);
+});
+
+socket.on("new user", number => {
+    updateUsersCount(number);
 });
--- server.js
+++ server.js
@@ -6,12 +6,27 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));//Servindo os arquivos
 
-io.on('connection', function (socket) {
-  socket.emit("new user", "A new user has connected!");
+/* Quando o usuário se conectar */
+io.on('connection', socket => {
+  /*
+  * Por algum motivo o socket só está emitindo para o sender
+  * Então por isso é necessário o uso dos dois emissores
+  */
+  socket.broadcast.emit("new user", io.engine.clientsCount);
+  socket.emit("new user", io.engine.clientsCount);
 
-  socket.on("chat message", msg => {
+  socket.on("typing", user => {
+    socket.broadcast.emit("typing", user);
+  });
+
+  socket.on('')
+
+  /* emmiting for all users except the sender */
+  socket.on("chat message", (msg, user) => {
     socket.broadcast.emit("message for all users", msg);
   });
+
+  socket.on('disconnecting', () => socket.broadcast.emit("new user", io.engine.clientsCount));
 });
 
 httpServer.listen(process.env.PORT || 3000);//Caso seja acessado pelo Heroku
